eb97dcfc338b438ecfff92ab0596cea931bd0e2d
unknown <sikanderkhan002@gmail.com>
2025-07-03 00:12:38 +0530

    added PDAY3

---
+++ PDay3/Operators.py
@@ -0,0 +1,25 @@
+# print(20 and 10)
+# print(10 and 20)
+# print(20 or 10)
+# print(10 or 20)
+
+a=10
+b=20
+print("python"+"2")
+print(b/a)
+print(b//a)
+print(b%a)
+
+max_value= a  if a>b else b
+min_value= b  if b<a else a
+
+print(f"max value is {max_value}")
+print(f"min value is {min_value}")
+
+
+c=900
+d=900
+print(c is d)
+
+s="sikander"
+print("si" in s)
