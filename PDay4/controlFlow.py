18fdd020da3153d90f94404a2fa54e9b7c9c1667
unknown <sikanderkhan002@gmail.com>
2025-07-05 15:04:59 +0530

    controlFlow.py

---
+++ PDay4/controlFlow.py
@@ -0,0 +1,32 @@
+# a = int(input("Enter a number"))
+
+# if a == 10:
+#     print(f"this is \t{a}")
+
+
+# name = input("Enter name")
+# if name == 'sikander':
+#     print(f'He is {name}')
+# elif name == 'virat':
+#     print(f'He is {name}')
+# else :
+#     print('This is by default condition')         
+
+i=10
+while i>0:
+    print(f"Value of i is {i}")
+    i-=1
+
+print("\n")
+
+j=1
+while j<=10:
+    print(f"Value of j is {j}")
+    j+=1
+
+number = int(input("Enter number"))
+
+if number%2==0:
+    print(f"This number is even {number}")
+else :
+    print("This number is odd")    
